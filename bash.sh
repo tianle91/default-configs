8be699aee4580721796172b133d82e6fb6eec13a
Tianle Chen <tianlechen@gmail.com>
2020-06-28 10:22:43 -0400

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,12 @@
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# ensure pip version is python version
+alias pip='python -m pip'
+
+# ls
+alias ls='ls -hF'  # color, human-readable sizes, and file-type indicators.
+
+# kubectl
+alias ktl='kubectl'
