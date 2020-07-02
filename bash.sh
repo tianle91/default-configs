b8df4215deea9a4621d05af20704c7aec6a4b659
Tianle Chen <tianlechen@gmail.com>
2020-07-02 12:33:46 -0400

    remove shopt

--- bash.sh
+++ bash.sh
@@ -1,10 +1,6 @@
 # qol
 alias unzipallhere='for i in *.zip; do unzip "$i" -d "${i%%.zip}"; done'
 
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
-shopt -s checkwinsize
-
 # ensure pip version is python version
 alias pip='python -m pip'
 
