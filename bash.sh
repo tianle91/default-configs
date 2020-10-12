9b09cb07104d977de545973065aaff180fd1b942
Tianle Chen <tianlechen@gmail.com>
2020-10-12 08:38:10 -0400

    more python

--- bash.sh
+++ bash.sh
@@ -1,11 +1,12 @@
 # qol
 alias unzipallhere='for i in *.zip; do unzip "$i" -d "${i%%.zip}"&; done'
 
-# ensure pip version is python version
-alias pip='python -m pip'
-
 # ls
 alias ls='ls -hF'  # color, human-readable sizes, and file-type indicators.
 
 # kubectl
 alias k='kubectl'
+
+# python
+alias pip='python -m pip'
+alias jupyter='python -m jupyter'
