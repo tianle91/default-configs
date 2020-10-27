f28a4f0b73c7153417b50c005b34fc983d8e9290
Tianle Chen <tianlechen@gmail.com>
2020-10-27 14:45:15 -0400

    remove after unzip

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 # qol
-alias unzipallhere='for i in *.zip; do unzip "$i" -d "${i%%.zip}"&; done'
+alias unzipallhere='for i in *.zip; do unzip "$i" -d "${i%%.zip}"&& rm "$i"&; done'
 
 # ls
 alias ls='ls -hF'  # color, human-readable sizes, and file-type indicators.
