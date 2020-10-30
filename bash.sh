2c2bf5c69f86dbddf88ff2349d83f5cdfef12ed8
Tianle Chen <tianlechen@gmail.com>
2020-10-29 22:09:04 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -7,6 +7,10 @@ alias ls='ls -hF'  # color, human-readable sizes, and file-type indicators.
 # kubectl
 alias k='kubectl'
 
-# python
+# python virtually
 alias pip='python -m pip'
 alias jupyter='python -m jupyter'
+
+# python actually
+alias python3.7='/usr/local/opt/python@3.7/'
+alias virtualenv='python3 -m virtualenv'
